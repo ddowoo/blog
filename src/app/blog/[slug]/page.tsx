a058b562f64e11213f0b718eaef5a1fc2f6883c2
leedongwook-kr <63283076+leedongwook-kr@users.noreply.github.com>
2023-12-30 18:10:04 +0900

    chore: seo 셋팅
    google search console 셋팅, blog/[slug]/page metadata 생성

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,24 +1,25 @@
-"use client";
 import { Post, allPosts } from "contentlayer/generated";
+import { Metadata } from "next";
 import { useMDXComponent } from "next-contentlayer/hooks";
 import { notFound } from "next/navigation";
 
+export const generateMetadata = ({ params: { slug } }: { params: { slug: string } }): Metadata => {
+  const post = allPosts.find(({ _raw: { flattenedPath } }) => flattenedPath === slug) as Post;
+  return {
+    title: post.title,
+    description: post.description,
+    metadataBase: new URL("https://www.ddowoo.blog"),
+  };
+};
+
 export default function Post({ params: { slug } }: { params: { slug: string } }) {
   const post = allPosts.find(({ _raw: { flattenedPath } }) => flattenedPath === slug) as Post;
-
   if (!post) notFound();
 
   const MDXContent = useMDXComponent(post.body.code);
 
   return (
     <>
-      {/* <Head>
-        <meta property="og:title" content={post.title} />
-        <meta property="og:type" content="article" />
-        <meta property="og:description" content={post.description} />
-        <meta property="og:url" content="https://maintainhoon.vercel.app" />
-        <meta property="og:image" content="https://maintainhoon.vercel.app/images/metaLogo.png" />
-      </Head> */}
       <article className="mx-auto prose">
         <div className="mt-10 pb-10 border-b-2 prose dark:prose-invert">
           <h1 className="mb-16">{post.title}</h1>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,11 +4,13 @@ import Header from "../components/header";
 const inter = Inter({ subsets: ["latin"] });
 import "@/styles/global.css";
 import Footer from "@/components/footer";
-import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "DDOWWO TECH",
-  description: "프론트엔드 개발자 또우 기술 블로그",
+  description: "프론트엔드 개발자 또우 기술",
+  verification: {
+    google: "uOZl2hjFaHhmktVdunAnyXlgZLdeK0YEXt1NxRTmggw",
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
@@ -31,15 +33,14 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     }
 
     const colorMode = getUserColorMode();
-    console.log("colorMode : ", colorMode);
     document.body.className = colorMode;
   }
 
   return (
-    <html lang="en" className={inter.className}>
-      <body suppressHydrationWarning={true} className="dark">
+    <html lang="en" className={`${inter.className}`}>
+      <body suppressHydrationWarning={true} className="dark bg-slate-800 ">
         {/* <script dangerouslySetInnerHTML={{ __html: blockingSetUserColorMode }}></script> */}
-        <div className={`bg-white dark:bg-slate-800 dark:text-white min-h-dvh pb-5 pt-20`}>
+        <div className={`bg-white dark:bg-slate-800 dark:text-white min-h-dvh pb-5 pt-20 max-w-full`}>
           <Header />
           <main className="max-w-screen-lg px-5 m-auto relative">{children}</main>
           <Footer />
