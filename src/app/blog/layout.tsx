d5b551d40b69b3f0d9ebfb186e4a4449bf1fc43f
leedongwook-kr <63283076+leedongwook-kr@users.noreply.github.com>
2024-01-03 16:23:03 +0900

    chore:postList 컴포넌트 blog 하위 컴포넌트로 위치 변경

--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -1,4 +1,4 @@
-import PostList from "@/components/postList";
+import PostList from "@/app/blog/components/postList";
 import PostTagList from "@/components/postTagList";
 import { Suspense } from "react";
 
