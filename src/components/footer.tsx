af921c4c9bb9f66382da6570f051a6563c997565
leedongwook-kr <63283076+leedongwook-kr@users.noreply.github.com>
2024-02-12 19:15:57 +0900

    fix: footer layout 오류 수정

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="flex flex-col items-center w-full py-5 absolute bottom-0 dark:text-white bg-primary">
+    <footer className="flex flex-col items-center w-full py-5 absolute bottom-0 text-white">
       <div className="flex justify-center gap-4 items-center pt-4  w-36"></div>
     </footer>
   );
