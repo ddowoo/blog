078b3d4899b5b0aa8de91f03c66134ed25ecab8f
leedongwook-kr <63283076+leedongwook-kr@users.noreply.github.com>
2024-02-22 13:40:14 +0900

    fix : eslint 에러 수정

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
     <header className="w-full sticky top-0 py-5 w-100 z-50 bg-gray-900 px-5">
       <div className="max-w-screen-lg m-auto flex justify-between">
         <Link href="/" type="button" title="홈화면" className="my-auto">
-          <p className="dark:text-white font-bold">DDOWOO'S DOODLE</p>
+          <p className="dark:text-white font-bold">{`DDOWOO'S`} DOODLE</p>
         </Link>
         <div className="flex items-center">
           <Link href="/about" type="button" title="블로그">
